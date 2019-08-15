6c0103d3871e60082dc7324c0117e622c3512e84
hemanyuan <hemanyuan@offerxiansheng.com>
2019-08-15 12:09:32 +0800

    新增：热线电话

--- sensin-const.js
+++ sensin-const.js
@@ -1,3 +1,12 @@
+/*
+ * @Description:
+ * @Author: hemanyuan
+ * @Date: 2019-07-25 12:22:26
+ * @LastEditors: hemanyuan
+ * @LastEditTime: 2019-08-15 11:55:52
+ */
 export default {
   COMPANY: '成都先胜信息科技有限公司',
+  TEL: '028-65783563',
+  HOTLINE: '400-666-9860',
 };

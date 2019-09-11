08146259dccf80b336e945a7e14ac432d4ca1b63
刘剑 <liujian@magicare.me>
2019-09-11 10:13:34 +0800

    addlogo

--- sensin-const.js
+++ sensin-const.js
@@ -9,4 +9,6 @@ export default {
   COMPANY: '成都先胜信息科技有限公司',
   TEL: '028-65783563',
   HOTLINE: '400-666-9860',
-};
+  LOGO:
+    'https://resource-external.oss-cn-hangzhou.aliyuncs.com/static/logo/offer_120_120.png'
+}

3c94f91a4cb910b6bcd8387946f46abd1b235695
刘剑 <liujian@magicare.me>
2020-01-16 16:23:42 +0800

    修改电话号

--- sensin-const.js
+++ sensin-const.js
@@ -2,13 +2,12 @@
  * @Description:
  * @Author: hemanyuan
  * @Date: 2019-07-25 12:22:26
- * @LastEditors: hemanyuan
- * @LastEditTime: 2019-08-15 11:55:52
+ * @LastEditors  : liujians
+ * @LastEditTime : 2020-01-16 16:23:26
  */
 export default {
   COMPANY: '成都先胜信息科技有限公司',
-  TEL: '028-65783563',
+  TEL: '028-85229366',
   HOTLINE: '400-666-9860',
-  LOGO:
-    'https://resource-external.oss-cn-hangzhou.aliyuncs.com/static/logo/offer_120_120.png'
-}
+  LOGO: 'https://resource-external.oss-cn-hangzhou.aliyuncs.com/static/logo/offer_120_120.png',
+};
